e2048c709c23c5e57829451161d98c990be9f8f2
Thomas Bénas <benas.thomas@gmail.com>
2020-12-02 15:59:23 +0100

    strucutre de l'ajout de document

---
+++ include/tools.h
@@ -0,0 +1,8 @@
+#ifndef H_GL_TOOLS
+#define H_GL_TOOLS
+
+void printHelp(void);
+void printError(char *errStr);
+char* strlw(char *str);
+
+#endif
--- src/biblioManager.c
+++ src/biblioManager.c
@@ -1,7 +1,22 @@
+#include <string.h>
+#include <stdio.h>
+#include "tools.h"
+
 int isDocumentExist(char *doc){
     return 0;
 }
 
-void addDocument(void){
+void addDocument(char *type){
+
+    char *types[8] = {"book","article", "incollection", "inproceedings", "misc", "standard", "techreport", "these"};
+    char *t = strlw(type);
+
+    printf("*** Ajout d'un document %s ***\n", t);
     
+    if (strcmp(t, types[0]) == 0) //book
+    {
+          printf("Test réussi ! Si ! Si !\n");
+    } else{
+        printError(strcat(type," is an invalid document type."));
+    }
 }
