d88c8b71eecac20d518915f8199f1e37a13f7573
Thomas Bénas <benas.thomas@gmail.com>
2020-12-03 14:59:35 +0100

    ajout fonction dataEntriesDocument + commentaires

--- include/biblioManager.h
+++ include/biblioManager.h
@@ -1,7 +1,13 @@
 #ifndef H_GL_BIBLIOMANAGER
 #define H_GL_BIBLIOMANAGER
 
+/* Permet de dire si un document existe ou non dans la base */
 int isDocumentExist(char *doc);
+
+/* Permet d'ajouter un document à la base de données */
 void addDocument(char *type);
+
+/* Permet de lire les données saisies par l'utilisateur pour un document */
+void dataEntriesDocument(char *type);
 
 #endif
--- src/biblioManager.c
+++ src/biblioManager.c
@@ -1,9 +1,28 @@
 #include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "tools.h"
 
 int isDocumentExist(char *doc){
     return 0;
+}
+
+void dataEntriesDocument(char *type){
+
+    //on récupère le chemin absolu de la fiche type du document
+    char absolutePath[130];
+    strcpy(absolutePath, getDocumentTypePath(type));
+
+    FILE* file = fopen(absolutePath, "r");
+    char line[256];
+
+    while (fgets(line, sizeof(line), file)) {
+        printf("%s", line); 
+    }
+    printf("\n");
+
+    fclose(file);
+
 }
 
 void addDocument(char *type){
@@ -15,7 +34,7 @@ void addDocument(char *type){
     
     if (strcmp(t, types[0]) == 0) //book
     {
-          printf("Test réussi ! Si ! Si !\n");
+        dataEntriesDocument(t);
     } else{
         printError(strcat(type," is an invalid document type."));
     }
