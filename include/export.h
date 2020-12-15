e44a3638fcef808032c17a096887aca0680b2316
akgx99 <benas.thomas@gmail.com>
2020-12-15 11:03:42 +0000

    ajout fonction exportBiblio

--- include/export.h
+++ include/export.h
@@ -4,4 +4,7 @@
 /* Permet d'exporter des statistiques sur la bibliographie*/
 void showStats(char *folder, char *script);
 
+/*Permet d'exporter la bibliographie au format pdf ou txt*/
+void exportBiblio(char *ext);
+
 #endif
