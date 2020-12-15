6117313cc2f374cccd2e93fb4d6390b6995cb718
akgx99 <benas.thomas@gmail.com>
2020-12-15 11:04:04 +0000

    base fx exportBiblio

--- src/export.c
+++ src/export.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+#include "tools.h"
 
 void showStats(char *folder, char *script) {
     
@@ -8,4 +10,15 @@ void showStats(char *folder, char *script) {
 
     chdir(folder);
     system(script);
+}
+
+void exportBiblio(char *ext){
+    if(strcmp(ext, "pdf") == 0)
+    {
+        printf("*** Export de la bibliographie en pdf ***\n");
+    }
+    else if(strcmp(ext, "txt") == 0)
+    {
+        printf("*** Export de la bibliographie en txt ***\n");
+    }
 }
