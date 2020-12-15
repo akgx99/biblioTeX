da7b74f1579442474e3d98cfcc459a11deedc209
akgx99 <benas.thomas@gmail.com>
2020-12-15 11:04:34 +0000

    aération du code

--- src/main.c
+++ src/main.c
@@ -8,19 +8,22 @@ int main(int argc, char *argv[])
 
     //vérification du bon nombre d'argument
     if(argc < 2 || argc > 3){
+
         printError("Number of argument invalid");
         return EXIT_FAILURE;
     }
-
     if( strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "-help") == 0 || strcmp(argv[1], "--help") == 0) {  //-h -help --help
+
         printHelp();
         return EXIT_SUCCESS;
     }
     else if(strcmp(argv[1], "-add") == 0) {  //-add <type>
+
         addDocument(argv[2]);
         return EXIT_SUCCESS;
     }
     else if(strcmp(argv[1], "-delete") == 0) {  //-delete <document>
+
         if (!isDocumentExist(argv[2]))
         {
             char *err = strcat(argv[2], " document to be deleted does not exist in the database.");;
