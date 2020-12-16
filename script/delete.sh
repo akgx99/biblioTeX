7e66a3903b91515cdc5c92f7ab5383631e6618e7
chantony <chantonyty@gmail.com>
2020-12-16 14:30:30 +0100

    commentaires

--- script/delete.sh
+++ script/delete.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 FICHLISTE=$1
-i=0
+i=0 #Compteur de lignes
 j=0
 k=0
-trouve=1
-while read line  
+trouve=1 #Vérification si on a déjà trouvé l'élément
+while read line  #Lecture des lignes des documents
 do   
       i=$(($i + 1))
-   if [[ $line =  *${2}* ]]
+   if [[ $line =  *${2}* ]] #Si la ligne contient la chaîne passée en paramètres
    then
         j=$i
         trouve=0
     fi
-    if [[ $line =  "}" ]] && [ $trouve = 0 ]
+    if [[ $line =  "}" ]] && [ $trouve = 0 ] #Si on a trouvé le document et que la ligne contient seulement "}"
         then
         k=$i
         trouve=1
     fi
 done < $1
 
-sed ${j},${k}'d' $1 > temp.bib
-cat temp.bib > $1
+sed ${j},${k}'d' $1 > temp.bib #Suppression des lignes à partir de l'élément trouvé jusqu'à la prochaine accolade dans un fichier temporaire
+cat temp.bib > $1 
 rm temp.bib
