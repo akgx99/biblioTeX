10cb0ef6ae226ad64e41e694fc4c78b1006b4d1c
ChantonyTY <chantonyty@gmail.com>
2020-12-17 16:05:48 +0100

    Merge branch 'main' of https://github.com/akgx99/biblioTeX into main

--- script/stats.sh
+++ script/stats.sh
@@ -1,9 +1,26 @@
-#!/bin/sh
+#!/bin/bash
+
+while read line  #Lecture des lignes des documents
+do   
+   if [[ $line =  *@* ]] #Si la ligne contient la chaîne passée en paramètres
+   then
+          i=$(($i + 1))
+    fi
+done < $1
+echo $i " éléments" > script/st.dat
 
 gnuplot -p << EOF
+
+plot 'script/stats.dat' with linespoints ls 1
+set title "éléments"
+set style data histogram
 set xlabel "xlabel"
 set ylabel "ylabel"
-set title "graphTitle"
-plot "stats.dat" with lines
+set xr [$xmin:$xmax]
+set yr [$ymin:$ymax]
+set xtic auto
+set ytic auto
+set nokey
+set output 'curve.png'
 EOF
 
